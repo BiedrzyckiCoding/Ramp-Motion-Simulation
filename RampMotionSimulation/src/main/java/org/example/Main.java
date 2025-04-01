fca9c949d518e33e6ee7b74f0fbd7c46418e4428
kuba biedrzycki <jakub.biedrzycki11@gmail.com>
2025-04-01 14:55:52 +0200

    new project structure that uses maven

---
+++ RampMotionSimulation/src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.data.category.DefaultCategoryDataset;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame("Line Chart Example");
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+
+            // Sample dataset
+            DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+            dataset.addValue(1.0, "Series1", "2020");
+            dataset.addValue(4.0, "Series1", "2021");
+            dataset.addValue(3.0, "Series1", "2022");
+            dataset.addValue(5.0, "Series1", "2023");
+
+            // Create chart
+            JFreeChart chart = ChartFactory.createLineChart(
+                    "Yearly Data",       // Chart title
+                    "Year",              // X-axis label
+                    "Value",             // Y-axis label
+                    dataset              // Dataset
+            );
+
+            ChartPanel chartPanel = new ChartPanel(chart);
+            frame.setContentPane(chartPanel);
+            frame.setVisible(true);
+        });
+    }
+}
